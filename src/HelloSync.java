002d474ab0d3c5d779792faccb05abaa14bc49bd
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-09-15 10:47:10 -0400

    Added a second output

--- src/HelloSync.java
+++ src/HelloSync.java
@@ -3,7 +3,12 @@ public class HelloSync {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Say hello
 		System.out.println("Hello from school");
+		
+		System.out.println("I can't stop saying hi!!");
+		
 
 	}
 
